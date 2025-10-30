36d703490e9edd8cac35ddb0ad732aaaa745f805
Avinash Jha <avinashjha3012@gmail.com>
2025-10-30 12:47:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
+
 // Routes
 app.use('/api', require('./routes/index.route'));
 
